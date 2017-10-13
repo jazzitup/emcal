161bbcada1280f673eb928f87149ec96b1a63f7b
Yongsun Kim <kimy@cern.ch>
2017-10-13 10:49:03 -0500

    Q/A

--- twikiText.py
+++ twikiText.py
@@ -1,10 +1,10 @@
-for j in [ 'DBN_6-BL_21', 'DBN_52-BL_21', 'DBN_57-BL_21', 'DBN_59-BL_21' ] :
-    print ('---++ *',j,'*', sep='')
-    print ('%COMMENT{type="bulletabove" button="Add comment"}%')
-    print ('%TABLE{columnwidths="25%,25%,25%,25%}%')
-    print('| [[%ATTACHURL%/',j,'-N.JPG][<img alt="', j, '-N.JPG" src="%ATTACHURLPATH%/', j, '-N.JPG" width="400" />]]  | [[%ATTACHURL%/', j, '-NG.JPG][<img alt="', j, '-NG.JPG" src="%ATTACHURLPATH%/', j, '-NG.JPG" width="400" />]] | [[%ATTACHURL%/', j, '-W.JPG][<img alt="', j, '-W.JPG" src="%ATTACHURLPATH%/', j, '-W.JPG" width="400" />]]  | [[%ATTACHURL%/', j, '-WG.JPG][<img alt="', j, '-WG.JPG" src="%ATTACHURLPATH%/', j, '-WG.JPG" width="400" />]]  |', sep='')
-    print ('</br></br></br>')
-    print ('\n')
+j = input('Enter the lable name. (e.g.) DBN_6-BL_21: ')
+print ('---++ *',j,'*', sep='')
+print ('%COMMENT{type="bulletabove" button="Add comment"}%')
+print ('%TABLE{columnwidths="25%,25%,25%,25%}%')
+print('| [[%ATTACHURL%/',j,'-N.JPG][<img alt="', j, '-N.JPG" src="%ATTACHURLPATH%/', j, '-N.JPG" width="400" />]]  | [[%ATTACHURL%/', j, '-NG.JPG][<img alt="', j, '-NG.JPG" src="%ATTACHURLPATH%/', j, '-NG.JPG" width="400" />]] | [[%ATTACHURL%/', j, '-W.JPG][<img alt="', j, '-W.JPG" src="%ATTACHURLPATH%/', j, '-W.JPG" width="400" />]]  | [[%ATTACHURL%/', j, '-WG.JPG][<img alt="', j, '-WG.JPG" src="%ATTACHURLPATH%/', j, '-WG.JPG" width="400" />]]  |', sep='')
+print ('</br></br></br>')
+print ('\n')
 
 
 
